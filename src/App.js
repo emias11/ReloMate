da6365a152cf3679c6bdbfd9dfb3d75777ea6e38
emias11 <mia.stapleton1@gmail.com>
2023-06-02 00:33:40 +0100

    Made changes to app

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import {
 } from "@react-google-maps/api";
 import { useRef, useState } from "react";
 
-const center = { lat: 48.8584, lng: 2.2945 };
+const center = { lat: 51.4988, lng: -0.181718 };
 
 function App() {
   const { isLoaded } = useJsApiLoader({
@@ -30,8 +30,6 @@ function App() {
 
   const [map, setMap] = useState(/** @type google.maps.Map */ null);
   const [directionsResponse, setDirectionsResponse] = useState(null);
-  const [distance, setDistance] = useState("");
-  const [duration, setDuration] = useState("");
 
   /** @type React.MutableRefObject<HTMLInputElement> */
   const originRef = useRef();
@@ -42,8 +40,8 @@ function App() {
     return <SkeletonText />;
   }
 
-  async function calculateRoute() {
-    if (originRef.current.value === "" || destiantionRef.current.value === "") {
+  async function placeMarker() {
+    if (originRef.current.value === "") {
       return;
     }
     // eslint-disable-next-line no-undef
@@ -54,17 +52,8 @@ function App() {
       // eslint-disable-next-line no-undef
       travelMode: google.maps.TravelMode.DRIVING,
     });
+    <Marker position={{ lat: 51.4988, lng: -0.181718 }} />;
     setDirectionsResponse(results);
-    setDistance(results.routes[0].legs[0].distance.text);
-    setDuration(results.routes[0].legs[0].duration.text);
-  }
-
-  function clearRoute() {
-    setDirectionsResponse(null);
-    setDistance("");
-    setDuration("");
-    originRef.current.value = "";
-    destiantionRef.current.value = "";
   }
 
   return (
@@ -89,7 +78,7 @@ function App() {
           }}
           onLoad={(map) => setMap(map)}
         >
-          <Marker position={center} />
+          {/* <Marker position={center} /> */}
           {directionsResponse && (
             <DirectionsRenderer directions={directionsResponse} />
           )}
@@ -107,43 +96,21 @@ function App() {
         <HStack spacing={2} justifyContent="space-between">
           <Box flexGrow={1}>
             <Autocomplete>
-              <Input type="text" placeholder="Origin" ref={originRef} />
-            </Autocomplete>
-          </Box>
-          <Box flexGrow={1}>
-            <Autocomplete>
               <Input
                 type="text"
-                placeholder="Destination"
-                ref={destiantionRef}
+                placeholder="Where will you be working..."
+                ref={originRef}
               />
             </Autocomplete>
           </Box>
 
           <ButtonGroup>
-            <Button colorScheme="pink" type="submit" onClick={calculateRoute}>
-              Calculate Route
+            <Button colorScheme="pink" type="Place" onClick={placeMarker}>
+              Place
             </Button>
-            <IconButton
-              aria-label="center back"
-              icon={<FaTimes />}
-              onClick={clearRoute}
-            />
           </ButtonGroup>
         </HStack>
-        <HStack spacing={4} mt={4} justifyContent="space-between">
-          <Text>Distance: {distance} </Text>
-          <Text>Duration: {duration} </Text>
-          <IconButton
-            aria-label="center back"
-            icon={<FaLocationArrow />}
-            isRound
-            onClick={() => {
-              map.panTo(center);
-              map.setZoom(15);
-            }}
-          />
-        </HStack>
+        <HStack spacing={4} mt={4} justifyContent="space-between"></HStack>
       </Box>
     </Flex>
   );
