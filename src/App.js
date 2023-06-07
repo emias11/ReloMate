99509e07f5a7ee6bfe9dc8c3dfdefcbea6838e06
Luc <63516928+Luc1103@users.noreply.github.com>
2023-06-07 16:49:03 +0100

    Temporarily commented getting the lat long function

--- src/App.js
+++ src/App.js
@@ -63,14 +63,14 @@ function App() {
   }
 
   function getLatLong(address) {
-    var geocoder = new google.maps.Geocoder();
-    geocoder.geocode( { 'address': address}, function(results, status) {
-      if (status == google.maps.GeocoderStatus.OK) {
-        return results[0].geometry.location
-      } else {
-        console.log("Geocoding failed: " + status);
-      }
-    });
+    // var geocoder = new google.maps.Geocoder();
+    // geocoder.geocode( { 'address': address}, function(results, status) {
+    //   if (status == google.maps.GeocoderStatus.OK) {
+    //     return results[0].geometry.location
+    //   } else {
+    //     console.log("Geocoding failed: " + status);
+    //   }
+    // });
   }
 
   return (
